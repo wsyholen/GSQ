2d10e6e7f194d7116ad2bcdf192e344de324370f
holennnnnn <317776764@qq.com>
2019-04-30 09:10:17 +0800

    新增  对象是否为空判断

---
+++ GSQ-forum-common/src/main/java/com/sixtyrobbers/GSQ/fourm/common/util/CheckObj.java
@@ -0,0 +1,36 @@
+package com.sixtyrobbers.GSQ.fourm.common.util;
+
+import java.lang.reflect.Field;
+
+/**
+ * <pre>
+ * Explain:
+ * Author: holennnnnn_
+ * Create_Time: 2019/4/12 11:05
+ * Version: V1.0
+ * </pre>
+ */
+public class CheckObj {
+
+    /**
+     * <pre>
+     * Explain: 判断对象里面属性是否为空
+     * Author: holennnnnn_
+     * Create_Time: 2019/4/12 11:06
+     * Version: V1.0
+     * </pre>
+     */
+    public static boolean checkObjFieldIsNull(Object obj) throws IllegalAccessException {
+        boolean flag = true;
+        for (Field f : obj.getClass().getDeclaredFields()) {
+            //取消Java的权限控制检查
+            f.setAccessible(true);
+            if (f.get(obj) == null) {
+                flag = false;
+                return flag;
+            }
+        }
+        return flag;
+    }
+
+}
