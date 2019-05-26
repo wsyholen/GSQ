c3b8ffcfa23b62adc953e47f23f1608ac959aa7f
qiusuo-Drq <179282781@qq.com>
2019-05-26 20:17:10 +0800

    test

--- GSQ-forum-controller/src/main/java/com/sixtyrobbers/GSQ/fourm/controller/forumController/ForeignJurisdictionController.java
+++ GSQ-forum-controller/src/main/java/com/sixtyrobbers/GSQ/fourm/controller/forumController/ForeignJurisdictionController.java
@@ -1,13 +1,8 @@
 package com.sixtyrobbers.GSQ.fourm.controller.forumController;
 
-import com.alibaba.fastjson.JSONObject;
-import com.sixtyrobbers.GSQ.fourm.controller.entity.BaseResult;
-import com.sixtyrobbers.GSQ.fourm.controller.entity.ResponseCodeEnum;
+import com.sixtyrobbers.GSQ.fourm.service.entity.BaseResult;
 import com.sixtyrobbers.GSQ.fourm.service.entity.forum.request.ForeignJurisdictionReq;
-import com.sixtyrobbers.GSQ.fourm.service.entity.forum.request.JurisdictionReq;
-import com.sixtyrobbers.GSQ.fourm.service.entity.forum.response.JurisdictionRes;
 import com.sixtyrobbers.GSQ.fourm.service.forumService.ForeignJurisdictionService;
-import com.sixtyrobbers.GSQ.fourm.service.forumService.JurisdictionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 /**
  * @Description:    更新用户对外权限
@@ -44,15 +37,14 @@ public class ForeignJurisdictionController {
     @RequestMapping(value = "/V1.0/updateForeignJurisdictionUser", method = RequestMethod.POST)
     @ResponseBody
     public BaseResult login(@RequestBody ForeignJurisdictionReq foreignJurisdictionReq)throws Exception{
-        String result = null;
-           result = foreignJurisdictionService.updateForeignJurisdictionUser(foreignJurisdictionReq);
-        if (result.equals("1")){
-            return new BaseResult(true, ResponseCodeEnum.ERROR_CODE_FOREIGNJURISDICTION_SUCCESS.getCode(), ResponseCodeEnum.ERROR_CODE_MODIFY_SUCCESS.getValue(), "更新用户对外权限成功！");
-        }else if (result.equals("0")){
-            return new BaseResult(false, ResponseCodeEnum.ERROR_CODE_FOREIGNJURISDICTION_ERROR.getCode(), ResponseCodeEnum.ERROR_CODE_MODIFY_ERRORS.getValue(), "更新用户对外权限失败！");
-        }else if (result.equals("请确定用户ID是否存在!")){
-            return new BaseResult(false, ResponseCodeEnum.ERROR_CODE_USERID_ERROR.getCode(), ResponseCodeEnum.ERROR_CODE_PHONE_PASSWORD_ERROR.getValue(), result);
-        }
+        String result = foreignJurisdictionService.updateForeignJurisdictionUser(foreignJurisdictionReq);
+//        if (result.equals("1")){
+//            return new BaseResult(true, ResponseCodeEnum.ERROR_CODE_FOREIGNJURISDICTION_SUCCESS.getCode(), ResponseCodeEnum.ERROR_CODE_MODIFY_SUCCESS.getValue(), "更新用户对外权限成功！");
+//        }else if (result.equals("0")){
+//            return new BaseResult(false, ResponseCodeEnum.ERROR_CODE_FOREIGNJURISDICTION_ERROR.getCode(), ResponseCodeEnum.ERROR_CODE_MODIFY_ERRORS.getValue(), "更新用户对外权限失败！");
+//        }else if (result.equals("请确定用户ID是否存在!")){
+//            return new BaseResult(false, ResponseCodeEnum.ERROR_CODE_USERID_ERROR.getCode(), ResponseCodeEnum.ERROR_CODE_PHONE_PASSWORD_ERROR.getValue(), result);
+//        }
         return null;
     }
 
